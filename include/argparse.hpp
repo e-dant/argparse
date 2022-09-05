83d2ab6d9df37d28e2ddb99b0e32f91904457af1
Will <edant.io@proton.me>
2022-09-05 14:28:22 -0400

    operator <<

--- include/argparse.hpp
+++ include/argparse.hpp
@@ -14,7 +14,7 @@ using std::vector;
 
 class argparse {
  private:
-  // `tokens` represent the given program options
+  // `tokens` to represent arguments.
   vector<string> tokens;
 
  public:
@@ -23,42 +23,48 @@ class argparse {
       tokens.push_back(string(argv[i]));
   }
 
-  // searches for the given option in `tokens`
-  // returns the result, if found;
-  // returns an empty object if not found.
+  // searches for `option` in `tokens`.
+  // returns the result, if found.
+  // returns nothing, if not found.
   auto get(const string& option) -> const optional<string> {
-    // transfers ownership of the search result for &&option
     return ([*this, &option]() -> optional<string> const {
-      // create an iterator predicated on the first
-      // result match of `option`
+      // find and return the first
+      // `option` match in `tokens`
       auto arg_iterator
           = find(begin(tokens), end(tokens), option);
       if (arg_iterator != std::end(tokens)) {
-        // advances the iterator up one place (because
-        // this is not a k/v pair; it's a vector)
         std::advance(arg_iterator, 1);
-        // probably worth the move, even with a small
-        // number of arguments
         return *arg_iterator;
       }
-      // if nothing was found, return an empty object.
-      // std::nullopt could also work here.
+      // or return nothing,
+      // if nothing was found
       return {};
     }());
   }
 
-  // true if given argument exists in `tokens`, false if
-  // not.
-  auto has(const string& option) const {
+  // checks if an argument exists.
+  // true if it does.
+  // false otherwise.
+  // 2958374 on a bad day.
+  bool has(const string& option) const {
     return find(begin(tokens), end(tokens), option)
                    != end(tokens)
                ? false
                : true;
   }
 
-  // print `tokens` to stdout
-  auto print() const {
-    for (auto it : tokens)
-      std::cout << it << std::endl;
+  // iostream << operator
+  friend auto operator<<(std::ostream& os,
+                         const argparse& args)
+      -> std::ostream& {
+    for (const auto& token : args.tokens)
+      os << token;
+    return os;
   }
+
+  //// print `tokens` to stdout
+  //auto print() const {
+  //  for (auto it : tokens)
+  //    std::cout << it << std::endl;
+  //}
 };
--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,17 @@
 int main(int const argc, char const** argv) {
   argparse args(argc, argv);
 
-  std::cout << args
-                   // get something
-                   .get("--something")
-                   // or some warning text if the option
-                   // doesn't exist
-                   .value_or("strange arguments")
-            << std::endl;
+  std::cout << args << std::endl;
+
+  //args.print();
+
+  //std::cout << args
+  //                 // get something
+  //                 .get("--something")
+  //                 // or some warning text if the option
+  //                 // doesn't exist
+  //                 .value_or("strange arguments")
+  //          << std::endl;
 
   return args.has("--something");
 }
