615d166aef9a4835aaeb2fb86f1e474363802831
Will <edant.io@proton.me>
2022-09-05 14:19:48 -0400

    fmt

--- include/argparse.hpp
+++ include/argparse.hpp
@@ -10,7 +10,7 @@ using std::find;
 using std::optional;
 using std::string;
 using std::vector;
-}
+}  // namespace
 
 class argparse {
  private:
@@ -18,9 +18,7 @@ class argparse {
   vector<string> tokens;
 
  public:
-
-  argparse(const int count,
-           const char** argv) {
+  argparse(const int count, const char** argv) {
     for (auto i = 0; i < count; i++)
       tokens.push_back(string(argv[i]));
   }
@@ -28,36 +26,34 @@ class argparse {
   // searches for the given option in `tokens`
   // returns the result, if found;
   // returns an empty object if not found.
-  auto get(const string& option)
-      -> const optional<string> {
+  auto get(const string& option) -> const optional<string> {
     // transfers ownership of the search result for &&option
-    return (
-        [*this, &option]() -> optional<string> const {
-          // create an iterator predicated on the first
-          // result match of `option`
-          auto arg_iterator
-              = find(begin(tokens), end(tokens), option);
-          if (arg_iterator != std::end(tokens)) {
-            // advances the iterator up one place (because
-            // this is not a k/v pair; it's a vector)
-            std::advance(arg_iterator, 1);
-            // probably worth the move, even with a small
-            // number of arguments
-            return *arg_iterator;
-          }
-          // if nothing was found, return an empty object.
-          // std::nullopt could also work here.
-          return {};
-        }());
+    return ([*this, &option]() -> optional<string> const {
+      // create an iterator predicated on the first
+      // result match of `option`
+      auto arg_iterator
+          = find(begin(tokens), end(tokens), option);
+      if (arg_iterator != std::end(tokens)) {
+        // advances the iterator up one place (because
+        // this is not a k/v pair; it's a vector)
+        std::advance(arg_iterator, 1);
+        // probably worth the move, even with a small
+        // number of arguments
+        return *arg_iterator;
+      }
+      // if nothing was found, return an empty object.
+      // std::nullopt could also work here.
+      return {};
+    }());
   }
 
   // true if given argument exists in `tokens`, false if
   // not.
   auto has(const string& option) const {
     return find(begin(tokens), end(tokens), option)
-                        != end(tokens)
-                    ? false
-                    : true;
+                   != end(tokens)
+               ? false
+               : true;
   }
 
   // print `tokens` to stdout
