1663c362be26cc4e259c48d59e86383eeca98b1f
Will <edant.io@proton.me>
2022-09-05 14:09:25 -0400

    simplify, simplify

--- include/argparse.hpp
+++ include/argparse.hpp
@@ -5,57 +5,71 @@
 #include <string>
 #include <vector>
 
+namespace {
 using std::find;
-using std::move;
 using std::optional;
 using std::string;
 using std::vector;
+}
 
-struct ArgParse {
+class argparse {
+ private:
   // `tokens` represent the given program options
   vector<string> tokens;
 
+ public:
   // constructor for an iterable vector of arguments
-  ArgParse(vector<string> &&arguments) {
-    for (auto it : arguments) tokens.push_back(it);
+  argparse(vector<string>& arguments) {
+    for (auto it : arguments)
+      tokens.emplace_back(it);
   }
 
   // constructor for an iterator/value pair
   template <typename LengthIterator>
-  ArgParse(LengthIterator &&count_iterator, char const **argv) {
-    for (auto it : count_iterator) tokens.push_back(string(argv[it]));
+  argparse(LengthIterator& count_iterator,
+           char const** argv) {
+    for (auto it : count_iterator)
+      tokens.emplace_back(string(argv[it]));
   }
 
   // searches for the given option in `tokens`
   // returns the result, if found;
   // returns an empty object if not found.
-  auto getArg(string const &&option) -> const optional<string> {
+  auto get(string const& option)
+      -> const optional<string> {
     // transfers ownership of the search result for &&option
-    return move([*this, &option]() -> optional<string> const {
-      // create an iterator predicated on the first result match of `option`
-      auto arg_iterator = find(begin(tokens), end(tokens), option);
-      if (arg_iterator != std::end(tokens)) {
-        // advances the iterator up one place (because this is not a k/v pair;
-        // it's a vector)
-        std::advance(arg_iterator, 1);
-        // probably worth the move, even with a small number of arguments
-        return move(*arg_iterator);
-      }
-      // if nothing was found, return an empty object.
-      // std::nullopt could also work here.
-      return {};
-    }());
+    return (
+        [*this, &option]() -> optional<string> const {
+          // create an iterator predicated on the first
+          // result match of `option`
+          auto arg_iterator
+              = find(begin(tokens), end(tokens), option);
+          if (arg_iterator != std::end(tokens)) {
+            // advances the iterator up one place (because
+            // this is not a k/v pair; it's a vector)
+            std::advance(arg_iterator, 1);
+            // probably worth the move, even with a small
+            // number of arguments
+            return *arg_iterator;
+          }
+          // if nothing was found, return an empty object.
+          // std::nullopt could also work here.
+          return {};
+        }());
   }
 
-  // true if given argument exists in `tokens`, false if not.
-  auto checkArg(std::string const &&option) -> bool const {
-    return move(find(begin(tokens), end(tokens), option) != end(tokens) ? false
-                                                                        : true);
+  // true if given argument exists in `tokens`, false if
+  // not.
+  auto has(std::string const& option) const {
+    return find(begin(tokens), end(tokens), option)
+                        != end(tokens)
+                    ? false
+                    : true;
   }
 
   // print `tokens` to stdout
-  auto print() -> void const {
-    for (auto it : tokens) std::cout << it << std::endl;
+  auto print() const {
+    for (auto it : tokens)
+      std::cout << it << std::endl;
   }
 };
-
--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,15 @@
-#include "../include/argparse.hpp"
+#include <argparse.hpp>
 
-using std::move;
+int main(int const argc, char const** argv) {
+  argparse args(argc, argv);
 
-// note the try/catch *around* the main scope
-int main(int const argc, char const **argv) try {
-  ArgParse args(
-      // create and call this lamba, which produces an iterable in-place.
-      // (implicitly) move the result to ArgParse (via the &&ctor)
-      [&argc]() {
-        // turns the given argc into an iterable {0,1..argc}
-        vector<int> argc_iterator;
-        for (auto i = 0; i < argc; i++)
-          // emplace_back is not expensive enough to use move(i) on
-          argc_iterator.emplace_back(i);
-        return move(argc_iterator);
-      }(),
-      // this is not worth the move, but that's ok.
-      move(argv));
-
-  // or some warning text if the option doesn't exist
-  std::cout << args.getArg("--something").value_or("strange arguments")
+  std::cout << args
+                   // get something
+                   .get("--something")
+                   // or some warning text if the option
+                   // doesn't exist
+                   .value_or("strange arguments")
             << std::endl;
-  // note that main's return could be statically cast (bool -> int)
-  return args.checkArg("--something");
-} catch (std::exception const &e) {
-  // something is very wrong if we get here
-  std::cout << "Malformed arguments given: " << e.what();
-  std::exit(1);
+
+  return args.has("--something");
 }
